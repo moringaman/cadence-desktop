edaa8f129891b467319e4e87cf54271478d89259
Rob Clarke <leon@moringamutual.org>
2018-12-14 15:30:09 +0000

    image size optimization

--- src/renderer/store/modules/Copy.js
+++ src/renderer/store/modules/Copy.js
@@ -1,14 +1,15 @@
 
 
 const actions = {
-  copyCDN({commit} , {cdn, clipboard, notify}) {
+  copyCDN({commit, dispatch} , {cdn, clipboard, notify}) {
     var name = cdn.split('/').pop(),
     ext = cdn.split('.').pop(),
     link 
     ext === "js" ? link = `<script src="${cdn}"><\/script>`: 
     link = `<link rel="stylesheet" type="text/css" href="${cdn}">`
     clipboard(link); 
-    commit('setNotification', { msg:`Copied script tag for: ${name} to your clipboard to be pasted into your webpage`, color: 'success'} ) //maybe in component?
+    dispatch('notificationCtrl', { msg:`Copied script tag for: ${name} to your clipboard to be pasted into your webpage`, color: 'success'} ) //maybe in component?
+
   }
 }
 
