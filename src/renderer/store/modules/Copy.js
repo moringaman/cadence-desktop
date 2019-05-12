a006641186d9089302c7af7843ae71a22a5a6c73
moringaman <leon@moringamutual.org>
2019-05-12 19:43:03 +0100

    added css script tag

--- src/renderer/store/modules/Copy.js
+++ src/renderer/store/modules/Copy.js
@@ -8,13 +8,24 @@ const actions = {
     ext === "js" ? link = `<script src="${cdn}"><\/script>`: 
     link = `<link rel="stylesheet" type="text/css" href="${cdn}">`
     if (origin != null) {
-      let scriptTag = `<script>
+      if (ext === "js"){
+      var scriptTag = `<script>
       if (navigator.onLine) {
         document.write('<script src="${origin}">\x3C/script>')
       } else {
         document.write('<script src="${cdn}">\x3C/script>')
       }
       </script>`
+      } else {
+        // Write alternate scriptTag for css link
+      var scriptTag = `<script>
+      if (navigator.onLine) {
+        document.write('<link rel="stylesheet" type="text/css" href="${origin}">\x3C/link>')
+      } else {
+        document.write('<link rel="stylesheet" type="text/css" href="${cdn}">\x3C/link>')
+      }
+      </script>`
+      }
       clipboard(scriptTag)
       dispatch('notificationCtrl', { msg:`Copied script tag for: ${name} to your clipboard to be pasted into your webpage`, color: 'success'} ) //maybe in component?
     } else {
